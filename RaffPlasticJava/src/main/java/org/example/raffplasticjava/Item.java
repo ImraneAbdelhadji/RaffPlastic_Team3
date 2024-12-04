b0643d4f6ef1fe027430c5e156b7b493b1e017ac
ImraneAbdelhadji <146210627+ImraneAbdelhadji@users.noreply.github.com>
2024-12-04 13:52:57 +0100

    /

--- RaffPlasticJava/src/main/java/org/example/raffplasticjava/Item.java
+++
@@ -1,8 +0,0 @@
-package org.example.raffplasticjava;
-
-public interface Item {
-    int getID();
-    String getType();
-    double getHoeveelheid();
-    String toString(); // Zorg ervoor dat elke implementatie een goede string representatie heeft
-}
