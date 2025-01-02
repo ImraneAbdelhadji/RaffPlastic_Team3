548a70d9d7772680fe42ed305aff3abd5751d646
lucieokd <146327172+lucieokd@users.noreply.github.com>
2025-01-02 15:49:52 +0100

    Update Login.java

--- RaffPlasticJava/src/main/java/org/example/raffplasticjava/Login.java
+++ RaffPlasticJava/src/main/java/org/example/raffplasticjava/Login.java
@@ -7,7 +7,7 @@ import javafx.scene.control.Button;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 
-public class LoginController {
+public class Login {
 
     @FXML
     private TextField txtUsername;
