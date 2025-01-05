5a829083952929969276e01c849fcddbf4cefb7f
IbrahimLamrabet <ibrahim.lamrabet@student.odisee.be>
2025-01-05 13:03:47 +0100

    commit

--- RaffPlasticJava/src/test/java/org/example/raffplasticjava/CucumberTestRunner.java
+++ RaffPlasticJava/src/test/java/org/example/raffplasticjava/CucumberTestRunner.java
@@ -11,7 +11,7 @@ import org.junit.runner.RunWith;
 @RunWith(CucumberWithSerenity.class)
 @CucumberOptions(
         features = "src/test/resources/features/", // Pad naar de feature-bestanden
-        glue = "org.example.raffplasticjava",      // Package met de step definitions
+        glue = "org.121example.raffplasticjava",      // Package met de step definitions
         plugin = {"pretty"}                        // Zorgt voor een leesbare uitvoer
 )
 public class CucumberTestRunner {
